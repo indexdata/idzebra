6b0c8f4e80bf16ab4e610d2541e008472d226990
Sebastian Hammer <quinn@indexdata.com>
1994-08-17 14:09:47 +0000

    Small changes

--- include/bfile.h
+++ include/bfile.h
@@ -3,28 +3,24 @@
  * All rights reserved.
  * Sebastian Hammer, Adam Dickmeiss
  *
- * $Log: bfile.h,v $
- * Revision 1.2  1994-08-17 13:32:33  adam
- * Use cache in dict - not in bfile.
- *
- * Revision 1.1  1994/08/16  16:16:02  adam
- * bfile header created.
- *
+ $Log: bfile.h,v $
+ Revision 1.3  1994-08-17 14:09:47  quinn
+ Small changes
+
  */
 
 #ifndef BFILE_H
 #define BFILE_H
-#include <util.h>
 
 typedef struct BFile_struct
 {
     int fd;
     int block_size;
-} *BFile;
+} *BFile, BFile_struct;
 
 int bf_close (BFile);
-BFile bf_open (const char *name, int block_size, int rw);
-int bf_read (BFile bf, int no, void *buf);
-int bf_write (BFile bf, int no, const void *buf);
+BFile bf_open (const char *name, int block_size, int wflag);
+int bf_read (BFile bf, int no, int offset, int num, void *buf);
+int bf_write (BFile bf, int no, int offset, int num, const void *buf);
 
 #endif
