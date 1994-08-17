49f85cf78262660f8b923d0d6cff9e58acac0ed9
Sebastian Hammer <quinn@indexdata.com>
1994-08-17 14:09:32 +0000

    Compiles cleanly (still only dummy.

--- bfile/bfile.c
+++ bfile/bfile.c
@@ -4,12 +4,16 @@
  * Sebastian Hammer, Adam Dickmeiss
  *
  * $Log: bfile.c,v $
- * Revision 1.1  1994-08-17 13:55:08  quinn
- * Deleted
+ * Revision 1.2  1994-08-17 14:09:32  quinn
+ * Compiles cleanly (still only dummy.
+ *
+ * Revision 1.1  1994/08/17  13:55:08  quinn
+ * New blocksystem. dummy only
  *
  */
 
 #include <util.h>
+#include <bfile.h>
 #include <sys/types.h>
 #include <fcntl.h>
 #include <unistd.h>
@@ -21,20 +25,22 @@ int bf_close (BFile bf)
     return(0);
 }
 
-BFile bf_open (const char *name, int block_size, int cache, wflag)
+BFile bf_open (const char *name, int block_size, int wflag)
 {
     BFile tmp = xmalloc(sizeof(BFile_struct));
 
     if ((tmp->fd = open(name, wflag ? O_RDWR : O_RDONLY, 0666)) < 0)
     {
         log(LOG_FATAL, "open: %s"); 
-	return(-1);
+	return(0);
     }
     return(tmp);
 }
 
-int bf_read (BFile bf, int no, int offset, int no, void *buf);
+int bf_read (BFile bf, int no, int offset, int num, void *buf)
 {
-    if (seek
 }
-int bf_write (BFile bf, int no, int offset, int no, const void *buf);
+
+int bf_write (BFile bf, int no, int offset, int num, const void *buf)
+{
+}
