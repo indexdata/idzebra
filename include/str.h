eb2bdda70aee57124ac4bcb3110f9280472d1dbd
Sebastian Hammer <quinn@indexdata.com>
1994-10-20 13:46:36 +0000

    String-management system

---
+++ include/str.h
@@ -0,0 +1,15 @@
+/*
+ * Copyright (C) 1994, Index Data I/S 
+ * All rights reserved.
+ * Sebastian Hammer, Adam Dickmeiss
+ *
+ * $Log: str.h,v $
+ * Revision 1.1  1994-10-20 13:46:36  quinn
+ * String-management system
+ *
+ */
+
+#ifndef STR_H
+#define STR_H
+
+#endif
