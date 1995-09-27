4f4057606fcc67b92895e575a6f3af2d5165b75c
Adam Dickmeiss <adam@indexdata.dk>
1995-09-27 12:21:25 +0000

    New function: recType_byName.

--- include/recctrl.h
+++ include/recctrl.h
@@ -4,7 +4,10 @@
  * Sebastian Hammer, Adam Dickmeiss
  *
  * $Log: recctrl.h,v $
- * Revision 1.2  1995-09-15 14:45:03  adam
+ * Revision 1.3  1995-09-27 12:21:25  adam
+ * New function: recType_byName.
+ *
+ * Revision 1.2  1995/09/15  14:45:03  adam
  * Retrieve control.
  *
  * Revision 1.1  1995/09/14  07:48:13  adam
@@ -58,5 +61,7 @@ typedef struct recType
     int  (*extract)(struct recExtractCtrl *ctrl);
     int  (*retrieve)(struct recRetrieveCtrl *ctrl);
 } *RecType;
+
+RecType recType_byName (const char *name);
 
 #endif
