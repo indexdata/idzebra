4048ec5a22c5217963ca9945722e6ecf2413ee16
Adam Dickmeiss <adam@indexdata.dk>
1995-12-05 11:24:51 +0000

    New version.

---
+++ include/zebraver.h
@@ -0,0 +1,14 @@
+/*
+ * Copyright (C) 1994-1995, Index Data I/S 
+ * All rights reserved.
+ * Sebastian Hammer, Adam Dickmeiss
+ *
+ * $Log: zebraver.h,v $
+ * Revision 1.1  1995-12-05 11:24:51  adam
+ * New version.
+ *
+ */
+
+#ifndef ZEBRAVER
+#define ZEBRAVER "1.0a2"
+#endif
