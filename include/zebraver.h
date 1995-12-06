10dd604b23feab51e04dbf72fe89ed2b3eb82320
Adam Dickmeiss <adam@indexdata.dk>
1995-12-06 16:05:50 +0000

    New version.

--- include/zebraver.h
+++ include/zebraver.h
@@ -4,11 +4,14 @@
  * Sebastian Hammer, Adam Dickmeiss
  *
  * $Log: zebraver.h,v $
- * Revision 1.1  1995-12-05 11:24:51  adam
+ * Revision 1.2  1995-12-06 16:05:50  adam
+ * New version.
+ *
+ * Revision 1.1  1995/12/05  11:24:51  adam
  * New version.
  *
  */
 
 #ifndef ZEBRAVER
-#define ZEBRAVER "1.0a2"
+#define ZEBRAVER "1.0a3"
 #endif
