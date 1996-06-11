c633dc31e974f0c8b375729c0a850ac18adb68f8
Sebastian Hammer <quinn@indexdata.com>
1996-06-11 10:53:16 +0000

    Relevance work.

--- include/rsrel.h
+++ include/rsrel.h
@@ -4,7 +4,10 @@
  * Sebastian Hammer, Adam Dickmeiss
  *
  * $Log: rsrel.h,v $
- * Revision 1.1  1995-09-08 14:52:09  adam
+ * Revision 1.2  1996-06-11 10:53:16  quinn
+ * Relevance work.
+ *
+ * Revision 1.1  1995/09/08  14:52:09  adam
  * Work on relevance sets.
  *
  */
@@ -25,6 +28,8 @@ typedef struct rset_relevance_parms
     ISAM is;
     ISAM_P  *isam_positions;
     int     no_isam_positions;
+    int no_terms;
+    int *term_no;
 } rset_relevance_parms;
 
 #endif
