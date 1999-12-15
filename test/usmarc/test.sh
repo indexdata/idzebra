4ffcc4caf5dae0a8a55ebb4b481003b764127c84
Adam Dickmeiss <adam@indexdata.dk>
1999-12-15 09:54:21 +0000

    Minor changes.

--- test/usmarc/test.sh
+++ test/usmarc/test.sh
@@ -1,3 +1,15 @@
 :
-../../index/zebraidx update records
-../../index/zebrasrv
+echo Loading Records
+if [ -x ../../bin/zmbolidx ]; then
+	../../bin/zmbolidx update records
+fi
+if [ -x ../../bin/zebraidx ]; then
+	../../bin/zebraidx update records
+fi
+echo Starting Server
+if [ -x ../../bin/zmbolsrv ]; then
+	../../bin/zmbolsrv
+fi
+if [ -x ../../bin/zebrasrv ]; then
+	../../bin/zebrasrv
+fi
