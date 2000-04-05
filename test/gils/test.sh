aad54b136f5e842bb1ba2e1dfd7736f8de90ef8c
Adam Dickmeiss <adam@indexdata.dk>
2000-04-05 10:04:28 +0000

    Modified test script.

--- test/gils/test.sh
+++ test/gils/test.sh
@@ -1,15 +1,15 @@
 :
 echo Loading Records
-if [ -x ../../bin/zmbolidx ]; then
-	../../bin/zmbolidx -t grs.sgml update records
+if [ -x ../../index/zmbolidx ]; then
+	../../index/zmbolidx -t grs.sgml update records
 fi
-if [ -x ../../bin/zebraidx ]; then
-	../../bin/zebraidx -t grs.sgml update records
+if [ -x ../../index/zebraidx ]; then
+	../../index/zebraidx -t grs.sgml update records
 fi
 echo Starting Server
-if [ -x ../../bin/zmbolsrv ]; then
-	../../bin/zmbolsrv
+if [ -x ../../index/zmbolsrv ]; then
+	../../index/zmbolsrv
 fi
-if [ -x ../../bin/zebrasrv ]; then
-	../../bin/zebrasrv
+if [ -x ../../index/zebrasrv ]; then
+	../../index/zebrasrv
 fi
