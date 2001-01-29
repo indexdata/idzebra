044461d6b9ed20023930fa761286914f0b404f4c
Adam Dickmeiss <adam@indexdata.dk>
2001-01-29 13:29:07 +0000

    Test script creates tmp and lock if necessary.

--- test/usmarc/test.sh
+++ test/usmarc/test.sh
@@ -1,15 +1,17 @@
-:
+#!/bin/sh
+test -d tmp || mkdir tmp
+test -d lock || mkdir lock
 echo Loading Records
-if [ -x ../../bin/zmbolidx ]; then
-	../../bin/zmbolidx update records
+if [ -x ../../index/zmbolidx ]; then
+	../../index/zmbolidx update records
 fi
-if [ -x ../../bin/zebraidx ]; then
-	../../bin/zebraidx update records
+if [ -x ../../index/zebraidx ]; then
+	../../index/zebraidx update records
 fi
 echo Starting Server
-if [ -x ../../bin/zmbolsrv ]; then
-	../../bin/zmbolsrv
+if [ -x ../../index/zmbolsrv ]; then
+	../../index/zmbolsrv
 fi
-if [ -x ../../bin/zebrasrv ]; then
-	../../bin/zebrasrv
+if [ -x ../../index/zebrasrv ]; then
+	../../index/zebrasrv
 fi
