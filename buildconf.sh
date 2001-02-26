f07a28f05c7edf99a2405461ad7730323b0a191f
Adam Dickmeiss <adam@indexdata.dk>
2001-02-26 21:21:50 +0000

    Updated.

--- buildconf.sh
+++ buildconf.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
-# $Id: buildconf.sh,v 1.2 2001-02-21 11:22:54 adam Exp $
+# $Id: buildconf.sh,v 1.3 2001-02-26 21:21:50 adam Exp $
 dir=`aclocal --print-ac-dir`
 if [ -f $dir/yaz.m4 ]; then
-	aclocal
+	aclocal || exit 1
 else
-	aclocal -I .
+	aclocal -I . || exit 1
 fi
-automake -a
-autoconf
+automake -a >/dev/null 2>&1 || exit 2
+autoconf || exit 3
