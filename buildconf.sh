12ed44cad4b6c14fd21384e26c077349af702fcb
Adam Dickmeiss <adam@indexdata.dk>
2001-02-28 09:01:41 +0000

    Fixed problem with missing isams for buildconf.sh.

--- buildconf.sh
+++ buildconf.sh
@@ -1,12 +1,15 @@
 #!/bin/sh
-# $Id: buildconf.sh,v 1.4 2001-02-26 22:14:59 adam Exp $
+# $Id: buildconf.sh,v 1.5 2001-02-28 09:01:41 adam Exp $
 dir=`aclocal --print-ac-dir`
 if [ -f $dir/yaz.m4 ]; then
 	aclocal || exit 1
 else
 	aclocal -I . || exit 1
 fi
-automake -a >/dev/null 2>&1 || exit 2
+test -d isamc || mkdir isamc
+test -d isamb || mkdir isamb
+test -d isam || mkdir isam
+automake -a >/dev/null 2>&1 || exit 2 
 autoconf || exit 3
 if [ -f config.cache ]; then
 	rm config.cache
