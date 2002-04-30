4d942c0e72457c0ffcab606a7ecaa72157bc7e45
Adam Dickmeiss <adam@indexdata.dk>
2002-04-30 21:07:56 +0000

    more verbose buildconf.sh

--- buildconf.sh
+++ buildconf.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
-# $Id: buildconf.sh,v 1.7 2002-04-05 08:46:26 adam Exp $
+# $Id: buildconf.sh,v 1.8 2002-04-30 21:07:56 adam Exp $
 dir=`aclocal --print-ac-dir`
 if [ -f $dir/yaz.m4 ]; then
 	aclocal
 else
 	aclocal -I .
 fi
-automake -a >/dev/null 2>&1 
+automake -a 
 autoconf
 if [ -f config.cache ]; then
 	rm config.cache
