f3ced7d9ab3c492a9f27678cddefea291b4e3bd4
Adam Dickmeiss <adam@indexdata.dk>
2002-06-20 08:03:30 +0000

    Better stat

--- test/dmoz/all.sh
+++ test/dmoz/all.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
-# $Id: all.sh,v 1.4 2002-06-19 10:29:18 adam Exp $
-./update.sh b
-./update.sh c
-./update.sh d
+# $Id: all.sh,v 1.5 2002-06-20 08:03:30 adam Exp $
+while true; do
+	./update.sh b
+	lpr times-b.ps
+	./update.sh c
+	lpr times-c.ps
+	./update.sh d
+	lpr times-d.ps
+done
--- test/dmoz/update.sh
+++ test/dmoz/update.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-# $Id: update.sh,v 1.7 2002-06-19 13:40:03 adam Exp $
+# $Id: update.sh,v 1.8 2002-06-20 08:03:30 adam Exp $
 t=$1
 test -n "$t" || exit 1
 rm -f *.mf *.LCK *.tmp
@@ -10,7 +10,15 @@ while test -f dmoz.$i.xml; do
 	echo -n "$i " >>times-$1.log
 	rm -f zebraidx-$t.log
 	../../index/zebraidx -l zebraidx-$t.log -c zebra-$t.cfg -f 10 update dmoz.$i.xml 
+
 	grep ' zebraidx times:' zebraidx-$t.log | sed 's/.*zebraidx times://g' >>times-$t.log
+
+	test -d rtmp || mkdir rtmp
+	cp *.mf rtmp
+	rm -f *.mf
+	mv rtmp/*.mf .
+
+	echo " ---------- $i ----- " >> stat-$t.log
 	../../index/zebraidx -l zebraidx-$t.log -c zebra-$t.cfg stat >>stat-$t.log 
 	i=`expr $i + 1`
 	if test $i = 30; then
