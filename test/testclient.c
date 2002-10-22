e868e46c150fd38e72a1d087ce5306bb7c715847
Adam Dickmeiss <adam@indexdata.dk>
2002-10-22 10:47:48 +0000

    Added testclient using ZOOM C from YAZ

---
+++ test/testclient.c
@@ -0,0 +1,41 @@
+/*
+ * $Id: testclient.c,v 1.1 2002-10-22 10:47:48 adam Exp $
+ *
+ * Synchronous single-target client doing search (but no yet retrieval)
+ */
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <yaz/xmalloc.h>
+#include <yaz/zoom.h>
+
+int main(int argc, char **argv)
+{
+    ZOOM_connection z;
+    ZOOM_resultset r;
+    int error;
+    const char *errmsg, *addinfo;
+
+    if (argc != 3)
+    {
+        fprintf (stderr, "usage:\n%s target query\n", *argv);
+        fprintf (stderr, " eg.  bagel.indexdata.dk/gils computer\n");
+        exit (1);
+    }
+    z = ZOOM_connection_new (argv[1], 0);
+    
+    if ((error = ZOOM_connection_error(z, &errmsg, &addinfo)))
+    {
+	fprintf (stderr, "Error: %s (%d) %s\n", errmsg, error, addinfo);
+	exit (2);
+    }
+
+    r = ZOOM_connection_search_pqf (z, argv[2]);
+    if ((error = ZOOM_connection_error(z, &errmsg, &addinfo)))
+	fprintf (stderr, "Error: %s (%d) %s\n", errmsg, error, addinfo);
+    else
+	printf ("Result count: %d\n", ZOOM_resultset_size(r));
+    ZOOM_resultset_destroy (r);
+    ZOOM_connection_destroy (z);
+    exit (0);
+}
