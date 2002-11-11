4ed7a26e63a0a8f537743b3c4c083c85fee4325a
Heikki Levanto <heikki@indexdata.dk>
2002-11-11 15:40:56 +0000

    A simple test with *one* sgml record

---
+++ test/xpath/simple1.sh
@@ -0,0 +1,24 @@
+# simple1.sh - test with one simple sgml record
+
+LOG=simple1.log
+../../index/zebraidx -l $LOG init || exit 1
+../../index/zebraidx -l $LOG update simple1.rec || exit 1
+
+echo "Starting server " >> $LOG
+../../index/zebrasrv -l $LOG -S unix:socket &
+sleep 1
+test -f zebrasrv.pid || exit 2
+
+../testclient unix:socket -c 0 '@attr 1=/sgml/tag before' >> $LOG || exit 1
+../testclient unix:socket -c 1 '@attr 1=/sgml/tag inside' >> $LOG || exit 1
+../testclient unix:socket -c 0 '@attr 1=/sgml/tag after'  >> $LOG || exit 1
+
+../testclient unix:socket -c 0 '@attr 1=/sgml/none after'  >> $LOG || exit 1
+
+../testclient unix:socket -c 1 '@attr 1=/sgml before' >> $LOG || exit 1
+../testclient unix:socket -c 1 '@attr 1=/sgml inside' >> $LOG || exit 1
+../testclient unix:socket -c 1 '@attr 1=/sgml after'  >> $LOG || exit 1
+
+echo "killing server " >> $LOG
+kill `cat zebrasrv.pid` || exit 3
+echo ok >> $LOG
