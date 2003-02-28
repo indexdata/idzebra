52fad02a1584f75a0c7ea56c5e3381f84c3f1ce0
Adam Dickmeiss <adam@indexdata.dk>
2003-02-28 10:44:44 +0000

    Create directories automatically

--- perl/demo/index.sh
+++ perl/demo/index.sh
@@ -1,4 +1,10 @@
 #!/bin/sh
-
-rm demo/register/* demo/lock/*
+if test ! -f demo/zebra.cfg; then
+	echo "Expected to find demo/zebra.cfg"
+	exit 1
+fi
+test -d demo/register || mkdir demo/register
+test -d demo/lock || mkdir demo/lock
+test -d demo/tmp || mkdir demo/tmp
+../index/zebraidx -c demo/zebra.cfg init
 ../index/zebraidx -c demo/zebra.cfg -g demo1 update /usr/lib/perl5
