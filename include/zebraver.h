340b7272ace5d3c17152c14a24eb8ab0eec4bf25
Adam Dickmeiss <adam@indexdata.dk>
2003-06-12 18:20:33 +0000

    Bump version

--- include/zebraver.h
+++ include/zebraver.h
@@ -1,4 +1,4 @@
-/* $Id: zebraver.h,v 1.33 2003-04-24 19:47:30 adam Exp $
+/* $Id: zebraver.h,v 1.34 2003-06-12 18:20:33 adam Exp $
    Copyright (C) 1995,1996,1997,1998,1999,2000,2001,2002,2003
    Index Data Aps
 
@@ -21,9 +21,9 @@ Free Software Foundation, 59 Temple Place - Suite 330, Boston, MA
 */
 
 #ifndef ZEBRAVER
-#define ZEBRAVER "1.3.11"
+#define ZEBRAVER "1.3.12"
 #endif
 
 #ifndef ZEBRADATE
-#define ZEBRADATE "$Date: 2003-04-24 19:47:30 $"
+#define ZEBRADATE "$Date: 2003-06-12 18:20:33 $"
 #endif
