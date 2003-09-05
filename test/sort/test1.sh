10ccfd152f58ba6d9ae9f958d8710656f1e3dfdd
Adam Dickmeiss <adam@indexdata.dk>
2003-09-05 12:33:39 +0000

    Fix test

--- test/sort/test1.sh
+++ test/sort/test1.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
-# $Id: test1.sh,v 1.3 2003-05-21 14:39:22 adam Exp $
+# $Id: test1.sh,v 1.4 2003-09-05 12:33:39 adam Exp $
+ulimit -c 10000
+set -x
 LOG=test1.log
 rm -fr lock
 mkdir lock
@@ -11,7 +13,7 @@ cp rec*.xml recs
 ../../index/zebraidx -l $LOG update recs || exit 1
 ../../index/zebrasrv -l $LOG unix:socket &
 sleep 1
-test -f zebrasrv.pid || exit 2
+test -f lock/zebrasrv.pid || exit 2
 ../api/testclient -n3 unix:socket '@or computer @attr 7=1 @attr 1=30 0' >tmp1
 echo 'Result count: 3
 my:
@@ -23,5 +25,5 @@ my:
   title: first computer
   dateTime: 2' >tmp2
 
-kill `cat zebrasrv.pid`
+kill `cat lock/zebrasrv.pid`
 diff tmp1 tmp2
