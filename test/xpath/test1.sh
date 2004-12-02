2fce83d6949f3117dfa6591cf6e9351a111b6ae1
Adam Dickmeiss <adam@indexdata.dk>
2004-12-02 12:08:39 +0000

    Remove unused stuff

--- test/xpath/test1.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-# $Id: test1.sh,v 1.5 2004-06-15 09:43:34 adam Exp $
-
-pp=${srcdir:-"."}
-
-LOG=test1.log
-rm -f $LOG
-../../index/zebraidx -c $pp/zebra.cfg -l $LOG init || exit 1
-../../index/zebraidx -c $pp/zebra.cfg -l $LOG -t grs.sgml update $pp/rec.xml || exit 2
