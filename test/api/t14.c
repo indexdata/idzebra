3e76d7533ae842c778fb8cd99e364bae25c361e9
Adam Dickmeiss <adam@indexdata.dk>
2005-12-15 13:28:46 +0000

    Added a test case for bug #447

---
+++ test/api/t14.c
@@ -0,0 +1,78 @@
+/* $Id: t14.c,v 1.1 2005-12-15 13:28:46 adam Exp $
+   Copyright (C) 2004-2005
+   Index Data ApS
+
+This file is part of the Zebra server.
+
+Zebra is free software; you can redistribute it and/or modify it under
+the terms of the GNU General Public License as published by the Free
+Software Foundation; either version 2, or (at your option) any later
+version.
+
+Zebra is distributed in the hope that it will be useful, but WITHOUT ANY
+WARRANTY; without even the implied warranty of MERCHANTABILITY or
+FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+for more details.
+
+You should have received a copy of the GNU General Public License
+along with Zebra; see the file LICENSE.zebra.  If not, write to the
+Free Software Foundation, 59 Temple Place - Suite 330, Boston, MA
+02111-1307, USA.
+*/
+
+#include "testlib.h"
+
+static void create_search_drop(ZebraHandle zh)
+{
+    const char *rec = "<gils><title>some</title></gils>";
+    const char *opaque_id = "9";
+    ZEBRA_RES res;
+
+    res = zebra_create_database (zh, "Default");
+    TL_ASSERT(res == ZEBRA_OK);
+
+#if 0
+    /* probably bug #447 .. but we don't know */
+    res = zebra_admin_exchange_record (
+	zh, rec, strlen(rec),
+	opaque_id, strlen(opaque_id),
+	1); /* insert */
+
+    TL_ASSERT(res == ZEBRA_FAIL);
+#endif
+
+    res = zebra_admin_exchange_record (
+	zh, rec, strlen(rec),
+	opaque_id, strlen(opaque_id),
+	4); /* update/insert */
+
+    TL_ASSERT(res == ZEBRA_OK);
+    do_query(__LINE__, zh, "@attr 1=4 some", 1);
+
+    zebra_drop_database(zh, "Default");
+
+    do_query_x(__LINE__, zh, "@attr 1=4 some", 0, 109);
+
+}
+
+int main(int argc, char **argv)
+{
+    ZebraService zs = start_up("zebra.cfg", argc, argv);
+    ZebraHandle zh = zebra_open(zs, 0);
+    ZEBRA_RES res;
+
+    res = zebra_select_database(zh, "Default");
+    TL_ASSERT(res == ZEBRA_OK);
+
+    zebra_init(zh);
+
+    create_search_drop(zh);
+#if 0
+    /* bug #447 */
+    create_search_drop(zh);
+#endif
+
+    return close_down(zh, zs, 0);
+}
+
+
