62af09d3a402d4edfbde306941601a978dfb45a0
Adam Dickmeiss <adam@indexdata.dk>
2006-02-06 13:19:55 +0000

    Test case for bug 460

--- test/marcxml/test1.sh
+++ test/marcxml/test1.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-# $Id: test1.sh,v 1.4.2.1 2004-08-24 14:06:32 adam Exp $
+# $Id: test1.sh,v 1.4.2.2 2006-02-06 13:19:55 adam Exp $
 
 pp=${srcdir:-"."}
 
@@ -12,11 +12,19 @@ if ../../index/zebraidx -c $pp/zebra.cfg -l $LOG -V|grep Expat >/dev/null; then
 else
 	exit 0
 fi
-../../index/zebraidx -c $pp/zebra.cfg -l $LOG $DBG update $pp/m*.xml
-../../index/zebrasrv -c $pp/zebra.cfg -l $LOG $DBG unix:socket &
+../../index/zebraidx -c $pp/zebra.cfg -l $LOG init
+../../index/zebraidx -c $pp/zebra.cfg -l $LOG update $pp/m*.xml
+../../index/zebrasrv -c $pp/zebra.cfg -l $LOG unix:socket &
+ret=0
 sleep 1
 ../api/testclient unix:socket '@and @attr 1=54 eng @and @attr 1=1003 jack @attr 1=4 computer' >tmp1
 echo 'Result count: 2' >tmp2
-kill `cat zebrasrv.pid` || exit 1
-diff tmp1 tmp2 || exit 2
+diff tmp1 tmp2 || ret=1
 rm -f tmp1 tmp2
+# bug # 460
+../api/testclient unix:socket '@attr 1=1003 a' >tmp1
+echo 'Result count: 0' >tmp2
+diff tmp1 tmp2 || ret=1
+rm -f tmp1 tmp2
+kill `cat zebrasrv.pid` || exit 1
+exit $ret
