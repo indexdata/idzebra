58b1837132acdc0f4683fef187b6a5b06534fa85
Adam Dickmeiss <adam@indexdata.dk>
2006-11-10 13:10:31 +0000

    Test for bug #730.

---
+++ test/xslt/xslt5.c
@@ -0,0 +1,74 @@
+/* $Id: xslt5.c,v 1.1 2006-11-10 13:10:31 adam Exp $
+   Copyright (C) 1995-2006
+   Index Data ApS
+
+This file is part of the Zebra server.
+
+Zebra is free software; you can redistribute it and/or modify it under
+the terms of the GNU General Public License as published by the Free
+Software Foundation; either version 2, or (at your option) any later
+version.
+
+Zebra is distributed in the hope that it will be useful, but WITHOUT ANY
+WARRANTY; without even the implied warranty of MERCHANTABILITY or
+FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+
+*/
+
+#include <stdio.h>
+#include "testlib.h"
+
+static void tst(int argc, char **argv)
+{
+    char path[256];
+    char profile_path[256];
+    char record_buf[20000];
+    FILE *f;
+    size_t r;
+
+    ZebraService zs = tl_start_up(0, argc, argv);
+    ZebraHandle  zh = zebra_open(zs, 0);
+
+    tl_check_filter(zs, "alvis");
+
+    YAZ_CHECK(zebra_select_database(zh, "Default") == ZEBRA_OK);
+    
+    zebra_init(zh);
+
+    sprintf(profile_path, "%s:%s/../../tab", 
+            tl_get_srcdir(), tl_get_srcdir());
+    zebra_set_resource(zh, "profilePath", profile_path);
+
+    zebra_set_resource(zh, "recordType", "alvis.marcschema-one.xml");
+
+    sprintf(path, "%.200s/marc-missing-ns.xml", tl_get_srcdir());
+    f = fopen(path, "rb");
+    YAZ_CHECK(f);
+    if (f)
+    {
+        r = fread(record_buf, 1, sizeof(record_buf)-1, f);
+        fclose(f);
+        YAZ_CHECK(r > 2);
+        record_buf[r] = '\0';
+        
+        YAZ_CHECK_EQ(zebra_add_record(zh, record_buf, strlen(record_buf)),
+                     ZEBRA_FAIL);
+    }
+    YAZ_CHECK(tl_close_down(zh, zs));
+}
+
+TL_MAIN
+
+/*
+ * Local variables:
+ * c-basic-offset: 4
+ * indent-tabs-mode: nil
+ * End:
+ * vim: shiftwidth=4 tabstop=8 expandtab
+ */
+
