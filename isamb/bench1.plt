b5052763f9975c0446ff13905d4f8b3c5c56f132
Adam Dickmeiss <adam@indexdata.dk>
2006-12-10 11:54:11 +0000

    Produce .dat files with bench1. prefix

--- isamb/bench1.plt
+++ isamb/bench1.plt
@@ -2,7 +2,7 @@ set terminal postscript eps
 set output "bench1.eps"
 set xlabel "R"
 set ylabel "time"
-plot "1000x1000.dat" title "isam 1000" with linespoints, \
-     "100x10000.dat" title "isam 100" with linespoints, \
-     "10x100000.dat" title "isam 10" with linespoints, \
-     "1x1000000.dat" title "isam 1" with linespoints
+plot "bench1.1000.dat" title "isam 1000" with linespoints, \
+     "bench1.100.dat" title "isam 100" with linespoints, \
+     "bench1.10.dat" title "isam 10" with linespoints, \
+     "bench1.1.dat" title "isam 1" with linespoints
--- isamb/bench1.sh
+++ isamb/bench1.sh
@@ -1,13 +1,17 @@
 #!/bin/sh
-# $Id: bench1.sh,v 1.1 2006-12-09 08:03:57 adam Exp $
+# $Id: bench1.sh,v 1.2 2006-12-10 11:54:11 adam Exp $
 # Test for variations of number of trees.. Total number of entries is
 # Constant 1 mio
 CMD="./benchisamb -r 50"
 
-$CMD -i 1000 -n 1000 >1000x1000.dat
+$CMD -i 1000 -n 1000 >bench1.1000.dat
+ls -l *.mf
 sleep 2
-$CMD -i 100 -n 10000 >100x10000.dat
+$CMD -i 100 -n 10000 >bench1.100.dat
+ls -l *.mf
 sleep 2
-$CMD -i 10 -n 100000 >10x100000.dat
+$CMD -i 10 -n 100000 >bench1.10.dat
+ls -l *.mf
 sleep 2
-$CMD -i 1 -n 1000000 >1x1000000.dat
+$CMD -i 1 -n 1000000 >bench1.1.dat
+ls -l *.mf
