fb43f202f00184ecbfc6fe319530699140e97f6a
Adam Dickmeiss <adam@indexdata.dk>
2009-07-31 13:40:32 +0200

    Remove redundant code

--- index/indexplugin.h
+++ index/indexplugin.h
@@ -16,21 +16,3 @@ void addDriverFunction(indexList);
 void zebraIndexBuffer(ZebraHandle zh, char *data, int dataLength, enum zebra_recctrl_action_t action, char *name);
 
 #endif
-#ifndef INDEXPLUGINH
-#define INDEXPLUGINH
-
-#include "../index/index.h"
-#include <assert.h>
-
-
-typedef int (*indexList)(ZebraHandle zh, const char *driverArg, enum zebra_recctrl_action_t action);
-
-typedef struct 
-{
-	indexList idxList;
-} zebra_index_plugin_object;
-
-void addDriverFunction(indexList);
-void zebraIndexBuffer(ZebraHandle zh, char *data, int dataLength, enum zebra_recctrl_action_t action, char *name);
-
-#endif
