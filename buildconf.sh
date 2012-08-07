ba9a1dffee09bca88d8352a9fb15bd185e7679a8
Adam Dickmeiss <adam@indexdata.dk>
2012-08-07 13:40:11 +0200

    Use m4/id-config.sh

--- buildconf.sh
+++ buildconf.sh
@@ -1,82 +1,12 @@
 #!/bin/sh
 
-automake=automake
-aclocal=aclocal
-autoconf=autoconf
-libtoolize=libtoolize
-autoheader=autoheader
-
-test -d autom4te.cache && rm -r autom4te.cache
-test -d config || mkdir config
 if [ -d .git ]; then
     git submodule init
     git submodule update
 fi
-if [ "`uname -s`" = FreeBSD ]; then
-    # FreeBSD intalls the various auto* tools with version numbers
-    echo "Using special configuration for FreeBSD ..."
-    automake=automake19
-    aclocal="aclocal19 -I /usr/local/share/aclocal"
-    autoconf=autoconf259
-    libtoolize=libtoolize15
-    autoheader=autoheader259
-fi
 
-if [ "`uname -s`" = Darwin ]; then
-    echo "Using special configuration for Darwin/MacOS ..."
-    libtoolize=glibtoolize
-fi
+. m4/id-config.sh
 
-if $automake --version|head -1 |grep '1\.[4-7]'; then
-    echo "automake 1.4-1.7 is active. You should use automake 1.8 or later"
-    if [ -f /etc/debian_version ]; then
-        echo " sudo apt-get install automake1.9"
-        echo " sudo update-alternatives --config automake"
-    fi
-    exit 1
-fi
-
-set -x
-$aclocal -I m4
-if grep AC_CONFIG_HEADERS configure.ac >/dev/null; then
-    $autoheader
-fi
-$libtoolize --automake --force 
-$automake --add-missing 
-$autoconf
-set -
-if [ -f config.cache ]; then
-	rm config.cache
-fi
-
-enable_configure=false
-enable_help=true
-sh_flags=""
-conf_flags=""
-case $1 in
-    -d)
-	sh_cflags="-O0 -g -Wall -Wdeclaration-after-statement -Wstrict-prototypes"
-	sh_cxxflags="-g -Wall"
-	enable_configure=true
-	enable_help=false
-	shift
-	;;
-    -c)
-	sh_cflags=""
-	sh_cxxflags=""
-	enable_configure=true
-	enable_help=false
-	shift
-	;;
-esac
-
-if $enable_configure; then
-    if [ -n "$sh_cflags" ]; then
-	CFLAGS="$sh_cflags" CXXFLAGS="$sh_cxxflags" ./configure --disable-shared --enable-static $*
-    else
-	./configure $*
-    fi
-fi
 if $enable_help; then
     cat <<EOF
 
@@ -117,8 +47,8 @@ EOF
     if [ "`uname -s`" = FreeBSD ]; then
         cat <<EOF
 When building from a Git, you need these FreeBSD Ports:
-  autoconf259, automake19, libtool15, bison, tcl84,
-  docbook-xsl, libxml2, libxslt, g++-4.0, make
+  pkg_add -r autoconf268 automake111 libtool tcl84 \\
+             docbook-xsl libxml2 libxslt g++-4.0 make
 EOF
     fi
 fi
