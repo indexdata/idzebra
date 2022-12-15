9f4173f717dd02ba2a1667d1336aadc21066d226
Adam Dickmeiss <adam@indexdata.dk>
2022-12-15 17:32:26 +0100

    Missing include

--- util/zebramap.c
+++ util/zebramap.c
@@ -27,6 +27,7 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 #include <charmap.h>
 #include <attrfind.h>
 #include <yaz/yaz-util.h>
+#include <yaz/snprintf.h>
 
 #if YAZ_HAVE_ICU
 #include <yaz/icu.h>
