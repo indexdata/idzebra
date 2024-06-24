76d3592c29572c42fdddcc2bfc116b4cd7fa107c
Hugh McMaster <hugh.mcmaster@outlook.com>
2024-06-24 21:58:30 +1000

    index: Add missing stdlib.h inclusions (mod_alvis, mod_dom)

--- index/mod_dom.c
+++ index/mod_dom.c
@@ -21,6 +21,7 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 #include <config.h>
 #endif
 #include <stdio.h>
+#include <stdlib.h>
 #include <assert.h>
 #include <ctype.h>
 #include <stdarg.h>
