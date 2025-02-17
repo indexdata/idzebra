dfd386f14c76ff2907bcb97200446b97f3d46c0f
Adam Dickmeiss <adam@indexdata.dk>
2025-02-17 22:03:25 +0100

    Fix compilation with c23 #63 (#64)

--- index/zinfo.h
+++ index/zinfo.h
@@ -25,13 +25,6 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 #include <idzebra/api.h>
 #include "recindex.h"
 
-/* Compare string (ignore case) */
-#ifdef WIN32
-#define STRCASECMP      stricmp
-#else
-#define STRCASECMP      strcasecmp
-#endif
-
 YAZ_BEGIN_CDECL
 
 typedef enum {
